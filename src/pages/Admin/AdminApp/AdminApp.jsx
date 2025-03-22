f8cbcee43d63eccc32b58ff7594ec606997b962b
Arjunexe <arrjunvm12345@gmail.com>
2025-03-22 12:24:47 +0530

    test

--- src/pages/Admin/AdminApp/AdminApp.jsx
+++ src/pages/Admin/AdminApp/AdminApp.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import AdminSidebar from "../../../components/admin/AdminSidebar/AdminSidebar";
 
 function Admin() {
+  const navigate = useNavigate()
   // If it's not loggedIn navigate to login otherwise navigate to dashboard
+  useEffect(() =>{
+    navigate("/admin/dashboard")
+  },[])
   return (
     <div className="flex">
       <AdminSidebar />
