6ed459da9fc00b437350c51f8f0210ccc82f36e5
Arjunexe <arrjunvm12345@gmail.com>
2025-05-23 11:37:56 +0530

    commentWip

--- src/components/modals/commentModal/CommentModal.jsx
+++ src/components/modals/commentModal/CommentModal.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function CommentModal({closeCommentModal}) {
   return (
     <div className='w-screen h-screen top-0 left-0 bg-red-600 z-30' onClick={closeCommentModal}>
-        
+        <div className='w-32 h-20 bg-yellow-200' onClick={(e) => e.stopPropagation()}>
+
+        </div>
     </div>
   )
 }
